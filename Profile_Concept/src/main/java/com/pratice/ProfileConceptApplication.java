57e015f7485145caaaba6d21933641215bbd21fb
Yash Gillorkar <144929163+YashGillorkar@users.noreply.github.com>
2024-10-21 23:19:45 +0530

    Spring Boot Profile Concept

---
+++ Profile_Concept/src/main/java/com/pratice/ProfileConceptApplication.java
@@ -0,0 +1,13 @@
+package com.pratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileConceptApplication.class, args);
+	}
+
+}
