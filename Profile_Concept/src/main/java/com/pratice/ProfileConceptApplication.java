76fc8272273eb97c2113f6600ce98222f8e8529c
Yash Gillorkar <144929163+YashGillorkar@users.noreply.github.com>
2024-10-21 23:42:32 +0530

    Profile done in Programmatically

--- Profile_Concept/src/main/java/com/pratice/ProfileConceptApplication.java
+++ Profile_Concept/src/main/java/com/pratice/ProfileConceptApplication.java
@@ -6,8 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProfileConceptApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProfileConceptApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        // Create a SpringApplication instance
+        SpringApplication app = new SpringApplication(ProfileConceptApplication.class);
+        
+        // Set the profile before running the application
+        app.setAdditionalProfiles("dev");
+        
+        // Run the application
+        app.run(args);
+    }
 }
